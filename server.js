3a88397a80604e6ef362cac9b1798dc066a14456
Tomas <tomasbarriosguevara@gmail.com>
2024-09-13 08:57:29 -0500

    OptimizeCode

--- server.js
+++ server.js
@@ -17,13 +17,14 @@ let data = [
 app.use(express.json());
 app.get('/user-info/:id',(req,res)=>{
     // add a condition to check if req.params.id is not a number
+    const user = data[parseInt(req.params.id)-1];
     if(isNaN(req.params.id)){
         res.status(400).json({"message":'Invalid request'});
     }
-    if(parseInt(req.params.id)>data.length || parseInt(req.params.id)<1){
-        res.status(404).json({"message":'User not found'});
+    if(!user){
+        res.status(404).send({"message":'User not found'});
     }
-    const user = data[parseInt(req.params.id)-1];
+
     res.json(user);
 })
 
